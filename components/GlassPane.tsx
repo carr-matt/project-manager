979b3bc4bff78c5d3c56e8eddac3906903fc7b7a
Matt <carr.matthew.lee@gmail.com>
2023-02-14 15:07:54 -0600

    more components, fixed GlassPane class names

--- components/GlassPane.tsx
+++ components/GlassPane.tsx
@@ -4,7 +4,7 @@ const GlassPane = ({ children, className }) => {
   return (
     <div
       className={clsx(
-        "blur-sm rounded-2xl border-solid border-2 border-gray-200",
+        "glass rounded-2xl border-solid border-2 border-gray-200",
         className
       )}
     >
